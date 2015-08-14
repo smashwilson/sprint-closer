ddbed2083c71e309d047233d9b7c1baffe872a11
Ash Wilson <smashwilson@gmail.com>
2015-08-14 09:33:05 -0400

    Load profiles.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
+	"strings"
 
 	log "github.com/smashwilson/sprint-closer/Godeps/_workspace/src/github.com/Sirupsen/logrus"
 	"github.com/smashwilson/sprint-closer/Godeps/_workspace/src/github.com/codegangsta/cli"
@@ -12,12 +14,18 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sprint-closer"
 	app.Usage = "Close the current DevEx sprint"
+	app.Version = "0.0.0"
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "log, l",
-			Value: "INFO",
+			Value: "info",
 			Usage: "Logging level",
+		},
+		cli.StringFlag{
+			Name:  "profile, p",
+			Value: path.Join(os.Getenv("HOME"), ".trello.json"),
+			Usage: "Path to a JSON profile.",
 		},
 	}
 
@@ -27,10 +35,19 @@ func main() {
 }
 
 func run(c *cli.Context) {
-	levelName := c.String("log")
+	levelName := strings.ToLower(c.String("log"))
 	level, err := log.ParseLevel(levelName)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 	log.SetLevel(level)
+
+	p, err := LoadProfile(c.String("profile"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Profile: %#v\n", p)
 }
--- profile.go
+++ profile.go
@@ -1,8 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+
+	log "github.com/smashwilson/sprint-closer/Godeps/_workspace/src/github.com/Sirupsen/logrus"
+)
+
 // Profile is the JSON-serialized configuration.
 type Profile struct {
 	Key          string `json:"key"`
 	Token        string `json:"token"`
 	Organization string `json:"organization"`
+}
+
+// LoadProfile attempts to locate and load a profile from disk.
+func LoadProfile(path string) (*Profile, error) {
+	inf, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.WithField("profile path", path).Error("You have no profile yet!")
+		}
+
+		return nil, err
+	}
+
+	p := &Profile{}
+	err = json.NewDecoder(inf).Decode(p)
+	if err != nil {
+		return p, err
+	}
+
+	return p, nil
 }
